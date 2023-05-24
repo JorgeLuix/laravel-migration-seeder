e11ce33f331638fda4c6ea5f482a4d7637b11e5e
JorgeLuix <tec.jorge.29@gmai.com>
2023-05-24 14:58:35 +0200

    create db, table_trains

---
+++ database/migrations/2023_05_24_082815_create_houses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('houses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('addres', 100);
+            $table->string('city', 50);
+            $table->string('postal_code', 10);
+            $table->string('state', 50);
+            $table->smallInteger('square_meters')->unsigned();
+            $table->tinyInteger('rooms')->unsigned();
+            $table->tinyInteger('bathrooms')->unsigned();
+            $table->boolean('garage')->default(0);
+            $table->float('price', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('houses');
+    }
+};
